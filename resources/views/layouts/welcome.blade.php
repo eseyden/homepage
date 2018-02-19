80da20e18fab8ff85acb2620061c16c9e9217885
Eric Seyden <ericseyden@yahoo.com>
2018-02-18 22:17:05 -0700

    fix year in the footer

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -34,7 +34,7 @@
         <p class="float-right">
             <a href="#">Back to top</a>
         </p>
-        <p>&copy; Eric Seyden 20018, all images are my original work.</p>
+        <p>&copy; Eric Seyden {{ date('Y') }}, all images are my original work.</p>
         <p>Created with  <a href="https://getbootstrap.com/">Bootstrap 4</a> & <a href="https://laravel.com/">Laravel 5.6</a></p>
         <p>Now serving #<span id="counter"></span></p>
     </div>

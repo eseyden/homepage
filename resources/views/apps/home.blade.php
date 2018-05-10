ba0b646022e0be6635d8ff40f888a73839da88a3
Eric Seyden <ericseyden@yahoo.com>
2018-05-10 12:40:38 -0600

    portfolio list

--- resources/views/apps/home.blade.php
+++ resources/views/apps/home.blade.php
@@ -1,1 +1,96 @@
 @extends('layouts.app')
+@section('content')
+    <h2>Notable App Development Portfolio</h2>
+    <ul>
+        <li>
+            <a href="m.umt.edu">University of Montana Mobile App</a>
+            <ul>
+                <li>Open Hours</li>
+                <li>Food Zoo Menu</li>
+            </ul>
+        </li>
+        <li>
+            Student Affairs IT
+            <ul>
+                <li>
+                    Direct Connect Office Home
+                    <ul>
+                        <li>Ticketing System</li>
+                        <li>Time Clocking</li>
+                        <li>Staff Position Management</li>
+                        <li>Retail Service POS</li>
+                    </ul>
+                </li>
+                <li>
+                    Email Queue Micro Service
+                </li>
+                <li>
+                    Commenting & File Attachment Micro Service
+                </li>
+            </ul>
+        </li>
+        <li>
+            ASUM
+            <ul>
+                <li>Transportation Bus Log</li>
+                <li>
+                    <a href="https://housing.apps.sa.umt.edu">
+                        Housing Finder
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            University Montana VETS Office
+            <ul>
+                <li>Notification of Intent to Use Benefits</li>
+                <li>
+                    Data-warehouse for Benefits Processing
+                    <ul>
+                        <li><a href="http://news.umt.edu/2015/03/030515umrd.php">2015 Outstanding Teamwork Award</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>University Center
+            <ul>
+                <li>Game Room Table Timer</li>
+            </ul>
+        </li>
+        <li>
+            Griz Card Center
+            <ul>
+                <li>Online Photo Submission</li>
+                <li>
+                    Unclaimed Property Reporting
+                    <ul>
+                        <li>
+                            <a href="http://c.ymcdn.com/sites/naccu.org/resource/resmgr/2018/awards/innovativetech_universityofm.pdf">2018 NACCU Innovative Technology Award</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>
+            UM Dining
+            <ul>
+                <li>
+                    <a href="https://www.umt.edu/dining/foodzoomenu/default.php">
+                        Food Zoo Online Menu
+                    </a>
+                </li>
+                <li>Resident Assistant Hall Snack Online Ordering</li>
+                <li>Bear Hugs Ordering</li>
+                <li>Student Staff Job Listings</li>
+            </ul>
+        </li>
+        <li>
+            UM Housing
+            <ul>
+                <li>Resident Assistant Duty Log</li>
+                <li>Student Staff Job Listings</li>
+            </ul>
+        </li>
+
+    </ul>
+@endsection
